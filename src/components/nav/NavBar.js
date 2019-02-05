b8d71952da806cf44af726648f35538b5fd9155a
dek <deeeqhaji@gmail.com>
2019-02-04 19:08:13 -0600

    added nav bar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -2,25 +2,25 @@ import React, { Component } from "react"
 import { Link } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./NavBar.css"
-import "bootstrap/dist/css/bootstrap.min.css"
+
 
 
 class NavBar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-light  light-grey flex-md-nowrap p-0 shadow">
+            <nav className="navbar navbar-dark  light-grey flex-md-nowrap p-0 shadow">
                 <ul className="nav nav-pills">
                     <li className="nav-item">
                         <Link className="nav-link" to="/">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="messages">Messages</Link>
+                        <Link className="nav-link" to="messages">/ Messages</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/friends">Friends</Link>
+                        <Link className="nav-link" to="/friends">/ Friends</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/log-out">Log out</Link>
+                        <Link className="nav-link" to="/log-out">/ Log out</Link>
                     </li>
                 </ul>
             </nav>
